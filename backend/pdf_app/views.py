f144c174705f321d70d49d558a3b0d5704438d55
Andrés Murillo <dev.andresmurillo@gmail.com>
2024-12-20 12:25:31 -0500

    Edit and delete documents.

--- backend/pdf_app/views.py
+++ backend/pdf_app/views.py
@@ -5,6 +5,7 @@ from .serializers import DocumentoPDFSerializer, UsuarioPersonalizadoSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
+from rest_framework.exceptions import PermissionDenied
 
 class DocumentoPDFViewSet(viewsets.ModelViewSet):
     queryset = DocumentoPDF.objects.all()
@@ -14,6 +15,20 @@ class DocumentoPDFViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Asignar el remitente como el usuario autenticado
         serializer.save(remitente=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        # Verificar que el usuario autenticado sea el remitente del documento
+        documento = self.get_object()
+        if documento.remitente != request.user:
+            raise PermissionDenied("No tienes permisos para editar este documento.")
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        # Verificar que el usuario autenticado sea el remitente del documento
+        documento = self.get_object()
+        if documento.remitente != request.user:
+            raise PermissionDenied("No tienes permisos para eliminar este documento.")
+        return super().destroy(request, *args, **kwargs)
 
     @action(detail=True, methods=['patch'])
     def aprobar(self, request, pk=None):
